2c53b30ac0f6f1874977c35246610211ae95509e
Michael Sewell <msewell@convoy.com>
2019-08-20 13:45:05 -0700

    Formatting corrections

--- DeployBrewCask.sh
+++ DeployBrewCask.sh
@@ -2,5 +2,5 @@
 # Apple approved way to get the currently logged in user (Thanks to Froger from macadmins.org and https://developer.apple.com/library/content/qa/qa1133/_index.html)
 ConsoleUser="$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')"
 
- # Install Homebrew as the currently logged in user
- sudo -H -u $ConsoleUser brew tap caskroom/cask  </dev/null
+# Install Homebrew as the currently logged in user
+sudo -H -u $ConsoleUser brew tap caskroom/cask  </dev/null	
